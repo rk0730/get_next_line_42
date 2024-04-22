f816404014b54431061d8ab55bbb8762ac813cab
rk0730 <164816796+rk0730@users.noreply.github.com>
2024-04-22 23:55:57 +0900

    gnl 設計

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 //標準入力のテスト
 // int	main(void)
@@ -14,14 +16,54 @@
 // }
 
 //ファイルのテスト
+// int	main(void)
+// {
+// 	int		fd; 
+// 	char	*tmp;
+
+// 	fd = open("test.txt", O_RDONLY);
+// 	tmp = get_next_line(fd);
+// 	printf("%s", tmp);
+// 	close(fd);
+// 	return (0);
+// }
+
 int	main(void)
 {
-	int		fd; 
-	char	*tmp;
+	// int	fd1 = open("alpha.txt", O_RDONLY);
+	// int	fd2 = open("alpha2.txt", O_RDONLY);
+	// int	fd3 = open("num.txt", O_RDONLY);
+	// char	*str = calloc(10, 1);
 
-	fd = open("test.txt", O_RDONLY);
-	tmp = get_next_line(fd);
-	printf("%s", tmp);
-	close(fd);
+	// ssize_t result = read(fd1, str, 8);
+	// printf("%s\n", str);
+	// printf("each\n");
+	// for (size_t i = 0; i < 8; i++)
+	// {
+	// 	printf("%d\n", str[i]);
+	// }
+	
+	// printf("result %zd\n", result);
+	// // read(fd1, str, 3);
+	// // printf("%s\n", str);
+	// // read(fd2, str, 3);
+	// // printf("%s\n", str);
+	// // read(fd1, str, 3);
+	// // printf("%s\n", str);
+	// // read(fd2, str, 3);
+	// // printf("%s\n", str);
+	// // read(0, str, 5);
+	// // printf("%s\n", str);
+	// // read(fd1, str, 3);
+	// // printf("%s\n", str);
+	// // read(0, str, 5);
+	// // printf("%s\n", str);
+
+	// free(str);
+	// close(fd1);
+	// close(fd2);
+	// close(fd3);
+	char *str = (char *)malloc(0);
+	printf("%p\n", str);
 	return (0);
 }
