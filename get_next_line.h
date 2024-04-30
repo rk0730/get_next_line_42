c2b8773bc03d5def4b73dbfa7238b723ef624cb8
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2024-04-30 11:40:09 +0900

    gnlできてる？

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rkitao <rkitao@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kitaoryoma <kitaoryoma@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 23:34:14 by kitaoryoma        #+#    #+#             */
-/*   Updated: 2024/04/29 21:51:59 by rkitao           ###   ########.fr       */
+/*   Updated: 2024/04/30 11:33:10 by kitaoryoma       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@
 # endif
 
 char	*get_next_line(int fd);
-char	*ft_gen_ans(int fd, char *box[OPEN_MAX]);
+char	*ft_gen_ans(int fd, char *box[OPEN_MAX + 1]);
 char	*ft_join_ans(char **old_ans_p, char *buf, size_t start, size_t end);
-void	ft_free_null(char *box[OPEN_MAX]);
+void	ft_free_null(char *box[OPEN_MAX + 1]);
 size_t	ft_strlen(const char *str);
 int		ft_gen_help(char *buf, char **ans_p, size_t *start_p, size_t *end_p);
 int		ft_find_nl(char *str);
